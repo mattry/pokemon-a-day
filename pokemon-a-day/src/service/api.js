9e1c9200d672bf78c2d3df0f87686f45a22393d3
Matt <84721683+mattry@users.noreply.github.com>
2025-02-21 13:46:53 -0500

    Define function to get the daily pokemon

--- pokemon-a-day/src/service/api.js
+++ pokemon-a-day/src/service/api.js
@@ -1,10 +1,26 @@
-// we will calculate the days since Jan. 1 to find the ID of the daily 'mon
-const today = new Date()
-const NYD = new Date(today.getFullYear(), 0, 1);
-// time in ms since start of year
-const diff = today - NYD;
-// convert the ms difference to difference in days
-// the id is 1 + difference in days
-const id = 1 + (Math.floor(diff / (1000 * 60 * 60 * 24)));
+import axios from "axios";
 
-console.log(id);
+const api = axios.create({
+    baseURL: 'http://localhost:3000/pokemon'
+});
+
+export default api;
+
+export const getTodaysMon = async () => {
+
+    // we will calculate the days since Jan. 1 to find the ID of the daily 'mon
+    const today = new Date()
+    const NYD = new Date(today.getFullYear(), 0, 1);
+    // time in ms since start of year
+    const diff = today - NYD;
+    // convert the ms difference to difference in days
+    // the id is 1 + difference in days
+    const id = 1 + (Math.floor(diff / (1000 * 60 * 60 * 24)));
+
+    return await api.get(`/${id}`);
+}
+
+
+
+
+
