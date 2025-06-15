f5b57d5f37d95f1450eb3b5a21096d58a4fe10f9
Matt <matthewr1995@yahoo.com>
2025-06-15 18:46:56 -0400

    Implement MUI components, update styling

--- pokemon-a-day/src/components/DisplayMon.jsx
+++ pokemon-a-day/src/components/DisplayMon.jsx
@@ -1,22 +1,110 @@
+import { Card, CardContent, CardMedia, Typography, Box, CardHeader, List, ListItem } from '@mui/material';
+import { useEffect } from 'react';
+
 const DisplayMon = ({ mon }) => {
+  const artworkUrl = mon?.sprites?.other?.['official-artwork']?.front_default;
+  const type = mon?.types?.map(t => t.type.name) || [];
 
-    const artworkUrl = mon?.sprites?.other?.['official-artwork']?.front_default;
-    const type = mon?.types?.map(t => t.type.name) || [];
+  const typeColors = {
+    normal: '#A8A878',
+    fire: '#c43d25',
+    water: '#6890F0',
+    electric: '#F8D030',
+    grass: '#78C850',
+    ice: '#98D8D8',
+    fighting: '#C03028',
+    poison: '#A040A0',
+    ground: '#E0C068',
+    flying: '#A890F0',
+    psychic: '#F85888',
+    bug: '#A8B820',
+    rock: '#B8A038',
+    ghost: '#705898',
+    dragon: '#7038F8',
+    dark: '#705848',
+    steel: '#B8B8D0',
+    fairy: '#F0B6BC',
+  };
 
-    if (!mon){
-        return(<p>Loading...</p>)
-      }
 
-    return(
-        <div id="card">
-            <div id="mon-name">
-                <h1>{mon.name}</h1>
-            </div>
-            <div id="image-box">
-                <img src={artworkUrl} alt="A pokemon" />
-            </div>
-        </div>
-    )
-}
+  const setBackground = (types) => {
+    const [type1, type2] = types;
+    const color1 = typeColors[type1] || '#ccc';
+    const color2 = typeColors[type2] || color1;
+
+    if (types.length === 1) {
+      return color1;
+    } else {
+      return `linear-gradient(135deg, ${color1} 50%, ${color2} 50%)`;
+    }
+  };
+
+  const background = setBackground(type);
+  
+  
+
+  if (!mon) {
+    return <Typography>Loading...</Typography>;
+  }
+
+  return (
+        <Card sx={{ 
+                maxWidth: 345, 
+                margin: 'auto', 
+                boxShadow: 3, 
+                marginTop: '2%',
+                border: '8px solid gold',
+                borderRadius: '10px',
+                backgroundColor: '#0034a3',
+            }}>
+            <CardHeader 
+                title={`${mon.name} #${mon.id}`}
+                sx={{ 
+                    textTransform: 'capitalize', 
+                    color: 'whitesmoke', 
+                    paddingBottom: 0,
+                    marginBottom: 0, 
+                    backgroundColor: '#0034a3',
+                }} 
+            />
+            <Box sx={{ border: '1px solid #ccc', borderRadius: 1, mx: 3, my: 2 }}>
+                <CardMedia
+                    component="img"
+                    height="200"
+                    image={artworkUrl}
+                    alt={`${mon.name} artwork`}
+                    sx={{
+                    objectFit: 'contain',
+                    background: background,
+                    borderRadius: 1,
+                    }}
+                />
+            </Box>
+            <CardContent sx={{mb: 8}}>
+                <List sx={{color: 'whitesmoke'}}>
+                    {/* height from API is in decimeters, convert to meters, to feet and inches */}
+                    <ListItem>
+                    Height: {(() => {
+                            const totalInches = mon.height * 3.937;
+                            const feet = Math.floor(totalInches / 12);
+                            const inches = Math.round(totalInches % 12);
+                            return `${feet}' ${inches}"`;
+                        })()
+                    }
+                    </ListItem>
+                    {/* weight from API is in hectograms, so we convert this to kg to pounds */}
+                    <ListItem>
+                        Weight: {(mon.weight * 0.1 * 2.20462).toFixed(1)} lbs.
+                    </ListItem>
+                </List>
+            {/* {type.map((t, idx) => (
+                <Typography key={idx} variant="body2" color="text.secondary">
+                    Type: {t}
+                </Typography>
+            ))} */}
+            </CardContent>
+        </Card>
+    );
+};
 
 export default DisplayMon;
--- pokemon-a-day/src/components/Header.jsx
+++ pokemon-a-day/src/components/Header.jsx
@@ -1,9 +1,15 @@
+import { AppBar, Toolbar, Typography } from '@mui/material';
+
 const Header = () => {
-    return(
-        <div id="header">
-            <h1>Pokemon-a-Day</h1>
-        </div>
-    )
-}
+  return (
+    <AppBar position="static" color="primary" sx={{ color: 'gold', backgroundColor: '#000BA3'}}>
+      <Toolbar>
+        <Typography variant="h4" component="div">
+          Pokemon-a-Day
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
