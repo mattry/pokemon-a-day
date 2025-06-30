b20a683336ad71399b043ebc9be779e5c4e55165
Matt <matthewr1995@yahoo.com>
2025-06-30 11:07:46 -0400

    Add pokeball svg to app bar

--- pokemon-a-day/src/components/Header.jsx
+++ pokemon-a-day/src/components/Header.jsx
@@ -1,14 +1,23 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import PokeballLogo from '../../pokeball.svg';
 
 const Header = () => {
   return (
-    <AppBar position="static" color="primary" sx={{ color: 'gold', backgroundColor: '#000BA3'}}>
-      <Toolbar>
-        <Typography variant="h4" component="div">
-          Pokemon-a-Day
-        </Typography>
-      </Toolbar>
-    </AppBar>
+      <AppBar position="static" color="primary" sx={{ color: 'gold', backgroundColor: '#000BA3' }}>
+        <Toolbar>
+          <Box display="flex" alignItems="center">
+            <Box
+              component="img"
+              src={PokeballLogo}
+              alt="Pokeball Logo"
+              sx={{ height: 40, width: 40, mr: 1 }}
+            />
+            <Typography variant="h4" component="div" sx={{ fontWeight: 'bold' }}>
+              Pokemon-a-Day
+            </Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
   );
 };
 
